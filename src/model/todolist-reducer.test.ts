27862f47a0e84117d6707f4c38ebea9f43fbc629
Vladislav <labkovich.vl@gmail.com>
2024-04-23 13:01:39 +0300

    feat: add test and remove todolist

---
+++ src/model/todolist-reducer.test.ts
@@ -0,0 +1,27 @@
+import {v1} from "uuid";
+import {TodolistType} from "../App";
+import {todolistReducer} from "./todolist-reducer";
+
+
+test('correct todolist should be removed', () => {
+
+    let todolistID1 = v1()
+    let todolistID2 = v1()
+
+    const startState: TodolistType[] = [
+        {id: todolistID1, title: 'What to learn ?', filter: 'all'},
+        {id: todolistID2, title: 'What to buy ?', filter: 'all'}
+    ]
+
+    const action = {
+        type: "REMOVE-TODOLIST",
+        payload: {
+            id: todolistID1
+        }
+    }
+
+    const endState = todolistReducer(startState, action)
+
+    expect(endState.length).toBe(1)
+    expect(endState[0].id).toBe(todolistID2)
+})
--- src/model/todolist-reducer.ts
+++ src/model/todolist-reducer.ts
@@ -6,8 +6,8 @@ type actionType = {
     payload: any
 }
 
-const todolistID1 = v1()
-const todolistID2 = v1()
+let todolistID1 = v1()
+let todolistID2 = v1()
 
 const initialState: TodolistType[]= [
     {id: todolistID1, title: 'What to learn ?', filter: 'all'},
@@ -17,7 +17,7 @@ const initialState: TodolistType[]= [
 export const todolistReducer = (state: TodolistType[] = initialState , action: actionType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
-            return state
+            return state.filter(el => el.id !== action.payload.id)
         }
         case 'ADD_TODOLIST': {
             return state
